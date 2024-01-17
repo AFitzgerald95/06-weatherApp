7ec364dc225e324e2385711a9cb476fbe988ec08
Austin Fitzgerald <132597919+AFitzgerald95@users.noreply.github.com>
2024-01-17 17:35:33 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
-// API Elements
 const apiKey = '332f3501fcaba0b14a6a6b60060dfea9';
 
-// DOM Elements
 let searchBtn = $('#searchBtn');
 let weatherDisplay = $('#weatherDisplay')
 let forecastDisplay = $('#forecastDisplay')
 let locationInput = $('#location')
 
-// Event listener for the form submission
 $('form').submit(function (event) {
     event.preventDefault();
-    // Get the user-entered location
     const userLocation = locationInput.val();
 
-    // Calls the function to fetch weather data
     getWeatherData(userLocation);
 
-    // Updates the recent searches
     updateRecentSearches(userLocation);
 });
 
@@ -24,7 +18,6 @@ function getWeatherData(location) {
     const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}`
 
-    // Fetch current weather data
     fetch(currentWeatherURL)
         .then(response => {
             if (!response.ok) {
@@ -33,7 +26,6 @@ function getWeatherData(location) {
             return response.json();
         })
         .then(data => {
-            // Function to update the current weather UI
             updateCurrentWeatherUI(data);
         })
         .catch(error => {
@@ -48,7 +40,6 @@ function getWeatherData(location) {
             return response.json();
         })
         .then(data => {
-            // Function to update the 5-day forecast UI
             updateForecastUI(data);
         })
         .catch(error => {
@@ -56,31 +47,27 @@ function getWeatherData(location) {
         });
 }
 
-// Function to convert Celsius to Fahrenheit
 function convertCelsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
 
-// Function to convert Kelvin to Celsius
 function kelvinToCelsius(kelvin) {
         return kelvin - 273.15;
 }
 
-// Function to update the UI with current weather data
 function updateCurrentWeatherUI(data) {
     console.log('Weather Data:', data);
 
-    // Updates the weatherDisplay with the relevant data
+    $('#weatherIcon').show();
+
     const kelvinTemp = data.main.temp;
     const celsiusTemp = Math.round(kelvinToCelsius(kelvinTemp));
 
-    // Adds a check to ensure temperature values are reasonable
     if (celsiusTemp > -100 && celsiusTemp < 100) {
         const fahrenheitTemp = convertCelsiusToFahrenheit(celsiusTemp);
 
 
-        // Updates with information about the current city
         $('#currentCityName').text(data.name);
         $('#currentDate').text(dayjs().format('MMMM D, YYYY'));
         $('#weatherIcon').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
@@ -92,16 +79,12 @@ function updateCurrentWeatherUI(data) {
     }
 }
 
-// Function to update the UI with 5-day forecast data
 function updateForecastUI(data) {
-    // Clears previous forecast data
     forecastDisplay.empty();
 
-    // Loops through forecast data
     for (let i = 0; i < data.list.length; i += 8) {
         const forecastItem = data.list[i];
 
-        // Extracts relevant information
         const timestamp = forecastItem.dt * 1000;
         const date = dayjs(timestamp).format('dddd, MMM D');
         
@@ -114,7 +97,6 @@ function updateForecastUI(data) {
         const iconCode = forecastItem.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
 
-        // Create HTML elements for forecast item
         const forecastItemHTML = `
             <li>
                 <strong>Date:</strong> ${date} |
@@ -126,43 +108,39 @@ function updateForecastUI(data) {
             </li>
         `;
 
-        // Appends the forecast item to the forecastDisplay container
         forecastDisplay.append(forecastItemHTML);
         }
     }
 
-// Function to update the UI with recent searches
 function updateRecentSearches(city) {
-    // Gets the existing searches from localStorage or creates an empty array
     const recentSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
 
-    // Adds the new city to the recent searches
     recentSearches.unshift(city);
 
-    // if statement that only keeps the 5 most recent searches
     if (recentSearches.length > 5) {
         recentSearches.pop();
     }
 
-    // Saves the updated recent searches to localStorage
     localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
 
-    // Updates the DOM with the recent searches
     const recentSearchesList = $('#recentSearches');
+    const searchHistoryTitle = $('#searchHistoryTitle');
+
+    if (recentSearches.length > 0) {
+        searchHistoryTitle.show();
+    } else {
+        searchHistoryTitle.hide();
+    }
+
     recentSearchesList.empty();
     recentSearches.forEach(search => {
         const listItem = $('<li>').text(search);
 
-        // Adds a click event listener to each list item
         listItem.click(function() {
-            // Sets the selected city in the search bar
             locationInput.val(search);
-            
-            // Triggers the form submission (simulates a user clicking the search button)
             $('form').submit();
         });
 
         recentSearchesList.append(listItem);
     });
 }
- 
